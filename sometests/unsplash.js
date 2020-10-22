7e561d9693f9923d7fdced38f6b1079b04f14565
mateomh <mmojicah.84@gmail.com>
2020-10-22 14:50:37 -0500

    Add test code for getting an image from the api

---
+++ sometests/unsplash.js
@@ -0,0 +1,24 @@
+async function getImage(type) {
+  let unsplashApiURL = 'https://api.unsplash.com/photos/random';
+  const clientID = 'yzaA27Bnm-RKqmb9ud2qC1imujfvoCREjcqG23C4ca0';
+  const orientation = 'landscape';
+  const count = 1;
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'Accept-Version': 'v1',
+    },
+  };
+
+  unsplashApiURL += `?orientation=${orientation}`;
+  unsplashApiURL += `&count=${count}`;
+  unsplashApiURL += `&client_id=${clientID}`;
+  unsplashApiURL += `&query=${type}`;
+  console.log('before the fetch');
+  const response = await fetch(unsplashApiURL, fetchOptions);
+  const [realData] = await response.json();
+  console.log(realData.urls.raw);
+  return realData.urls.raw;
+}
+
+getImage('cloudy');
