dcf7f64b68b2ad938593b97b906e02323632f98a
mateomh <mmojicah.84@gmail.com>
2020-10-23 09:46:19 -0500

    Add style sheet import to index.js so it can be handled by webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import * as Data from './data';
+import css from './styles.css';
+
+const header = document.getElementsByTagName('head')[0];
+const styleSheet = document.createElement('link');
+
+styleSheet.rel = 'stylesheet';
+styleSheet.type = 'text/css';
+styleSheet.href = css;
+
+header.appendChild(styleSheet);
 
 const infobtn = document.getElementById('send-button');
 infobtn.addEventListener('click', Data.getCityName);
